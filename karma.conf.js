f74673cbca7f990da0e8ccff7ea5438b50bd595c
Derek Ekins <derek@spathi.com>
2014-08-13 16:21:20 +0100

    added ie test target

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_DEBUG,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'IE'],
     singleRun: false
   });
 };
